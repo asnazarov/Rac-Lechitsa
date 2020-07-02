c83d695335cc090af402aa9c7870939485822848
Alexander Nazarov <asn.nazarov.asn@gmail.com>
2020-07-02 20:23:35 +0300

    initial

---
+++ pages/stories/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <logo></logo>
+    all stories
+  </div>
+</template>
+
+<script>
+import Logo from '@/components/Logo';
+export default {
+  components: {
+    Logo: Logo,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
