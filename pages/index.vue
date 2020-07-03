8db7c53f8010fc8c216b0e9221ef1e9834944d08
Alexander Nazarov <asn.nazarov.asn@gmail.com>
2020-07-03 07:28:02 +0300

    добавили шрифты Inter

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
     <h1 class="title">Hello</h1>
     <input type="text" name v-model="text" />
     <p class="subtitle">{{ text }}</p>
+    <a href="http://localhost:3000/stories">About</a>
   </div>
 </template>
 
@@ -34,10 +35,13 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  /* font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif; */
+    /* font-family: "Inter", "Source Sans Pro"; */
+    font-family: "Inter", sans-serif; 
   display: block;
-  font-weight: 300;
+  font-weight: 500;
+  /* font-weight: 300; */
   font-size: 100px;
   color: #35495e;
   letter-spacing: 1px;
