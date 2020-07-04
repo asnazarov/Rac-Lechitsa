a191bb34bf235877d52a4846006eee4d9ff4cecc
Alexander Nazarov <asn.nazarov.asn@gmail.com>
2020-07-04 18:41:17 +0300

    Подправили root-section

--- components/RootSection.vue
+++ components/RootSection.vue
@@ -4,15 +4,13 @@
       <h2 class="root-section__title">Истории людей, победивших рак, но не свои привычки</h2>
       <p class="root-section__text">Есть вещи, которые не лечатся. Вещи ставшие частью нашего «я», фобии, страхи. Но это точно не рак. Рак лечится. Лучшее доказательство — люди с их историями.</p>
       <nav-btn></nav-btn>
-      <!-- <div class="nav-btn">
-        <button class="nav-btn__left"></button>
-        <button class="nav-btn__right"></button>
-      </div> -->
     </div>
     <div class="root-section__video">
-     <iframe class="root-section__video-item"  src="https://www.youtube.com/embed/Qb4KUktbpzQ" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+     <iframe class="root-section__video-item"  src="https://www.youtube.com/embed/Qb4KUktbpzQ"
+     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+     allowfullscreen seamless></iframe>
       <figure>
-      <figcaption class="root-section__figcaption">Все видео вы можете найте на нашем <a class="root-section__fig-link" target="_blank" href="https://www.youtube.com/channel/UCcxMSzN1R4JfW1vLu3swCaQ/featured">YouTube канале</a>.</figcaption>
+        <figcaption class="root-section__figcaption">Все видео вы можете найте на нашем <a class="root-section__fig-link" target="_blank" href="https://www.youtube.com/channel/UCcxMSzN1R4JfW1vLu3swCaQ/featured">YouTube канале</a>.</figcaption>
       </figure>
     </div>
 
@@ -43,6 +41,8 @@ import NavBtn from '@/components/ui/NavBtn'
       display: flex;
       flex-direction: column;
       justify-content: space-between;
+      width: 40%;
+      margin-right: 15px;
 		}
 
 		&__title {
@@ -65,23 +65,34 @@ import NavBtn from '@/components/ui/NavBtn'
 		}
 
 		&__video {
-      max-width: 867px;
-      max-height: 450px;
+      width: 60%;
+      padding-bottom: calc(9 * 60% / 16);
+      position: relative;
+      align-self: flex-start;
     }
+
     &__video-item{
-      width: 867px;
-      height: 450px;
+      width: 100%;
+      height: 93%;
+      position: absolute;
+      top: 0;
+      left: 0;
       border-radius: 7px;
       border: none;
+
     }
 
 		&__figcaption {
+      position: absolute;
+      left: 0;
+      bottom: 0;
       font-family: 'Inter',sans-serif;
-      margin-top: 10px;
+      margin-top: 20px;
       font-size: 12px;
       line-height: 16px;
       color: #666666;
     }
+
     &__fig-link {
       font-family: 'Inter',sans-serif;
       color: #666666;
